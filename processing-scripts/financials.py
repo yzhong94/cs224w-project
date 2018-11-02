e532d0dd9affb9a9aed78c5d184f1800dedefed6
Eddie Chen <chenyang0320@gmail.com>
2018-11-02 17:11:09 +0800

    unified candidate nodeIDs based, and created committee node IDs based on where bill NodeIDs ended

--- processing-scripts/financials.py
+++ processing-scripts/financials.py
@@ -16,7 +16,8 @@ def loadCandidateMaster():
                  'CAND_NAME',
                  'CAND_PTY_AFFILIATION',
                  'CAND_ELECTION_YR',
-                 'CAND_OFFICE_ST',
+                 #'CAND_OFFICE_ST',
+                 'state',
                  'CAND_OFFICE',
                  'CAND_OFFICE_DISTRICT',
                  'CAND_ICI',
@@ -29,10 +30,16 @@ def loadCandidateMaster():
                  'CAND_ZIP']) for f in allFiles)
 	
 	#result = result[['CAND_ID','CAND_NAME', 'CAND_ELECTION_YR', 'CAND_OFFICE_ST', 'CAND_OFFICE']]
-	result = result[['CAND_ID','CAND_NAME']] 
-	result = result.groupby(['CAND_ID','CAND_NAME']).size().reset_index(name='Freq')
+	result = result[['CAND_ID','CAND_NAME','state']] 
+	result = result.groupby(['CAND_ID','CAND_NAME','state']).size().reset_index(name='Freq')
 
-	result["CanNodeId"] = result.reset_index().index
+	result['CAND_NAME'] = map(lambda x: x.upper(), result['CAND_NAME'])
+	result['state'] = map(lambda x: x.upper(), result['state'])
+
+	result['fname'], result['lname'] = result['CAND_NAME'].str.split(', ', 1).str
+	result['lname'], result['minitial'] = result['lname'].str.split(' ', 1).str
+
+	#result["CanNodeId"] = result.reset_index().index
 
 	#print(result.head())
 
@@ -127,10 +134,57 @@ def getDataPointsToPlot(Graph):
     ############################################################################
     return X, Y
 
+def loadCandidateNodeID():
+	filePathNode = "../processed-data/"
+	f = filePathNode + "legislator_node.csv"
+
+	canNode = pd.read_csv(f, delimiter= ",", index_col=False 
+                 #,names=[
+                 #'name',
+                 #'bioguide_id',
+                 #'state',
+                 #'Freq_x',
+                 #'thomas_id',
+                 #'Freq_y',
+                 #'NId']
+                 )
+
+	cm = loadCandidateMaster()
+	canNode['name'] = map(lambda x: x.upper(), canNode['name'])
+	canNode['state'] = map(lambda x: x.upper(), canNode['state'])
+
+	canNode['fname'], canNode['lname'] = canNode['name'].str.split(', ', 1).str
+	canNode['lname'], canNode['minitial'] = canNode['lname'].str.split(' ', 1).str
+
+	## Join on fname, lname, and state/CAND_OFFICE_ST
+	df = pd.merge(canNode, cm, on=['fname','lname','state'], how='left')
+
+	df = df[['fname','lname', 'name', 'state', 'CAND_ID', 'NId']]
+
+	df = df.groupby(['fname','lname', 'name', 'state', 'CAND_ID', 'NId']).size().reset_index(name='Freq')
+
+	print(df.shape)
+	print(df.head())
+
+	return df
+
+def findBillNodeOffset():
+
+	offset = 0
+
+	filePathNode = "../processed-data/"
+	f = filePathNode + "bill_node.csv"
+
+	bill = pd.read_csv(f, delimiter= ",", index_col=False)
+
+	offset = bill['NId'].apply(pd.to_numeric, errors='coerce').fillna(0).astype(np.int64).max()
+
+	return offset
+
 if __name__ == "__main__":
 
 	c = loadContributions()
-	can = loadCandidateMaster()
+	can = loadCandidateNodeID() #loadCandidateMaster()
 	com = loadCommMaster()
 
 	# merge dataframes together
@@ -141,27 +195,37 @@ if __name__ == "__main__":
 	c = c.fillna('')
 
 	#print(c.head())
-	c[['CanNodeId','ComNodeId']] = c[['CanNodeId','ComNodeId']].apply(pd.to_numeric, errors='coerce').fillna(0).astype(np.int64)
+	c[['NId','ComNodeId']] = c[['NId','ComNodeId']].apply(pd.to_numeric, errors='coerce').fillna(0).astype(np.int64)
 	
-	diff = max(c['CanNodeId']) + 1
-	c['ComNodeId'] = c['ComNodeId'] + diff
+	## remove entries where Nid = 0, meaning unsuccessful candidates
+	c = c[c.fname != '']
+
+	## add differential to commmittee node IDs, based on bill network
+	
+	d = findBillNodeOffset()
+	
+	#diff = max(c['NId']) + 1
+	#print(diff)
+
+	c['ComNodeId'] = c['ComNodeId'] + d
 
 	print(c.head())
 
+	## create an undirect graph
 	G = snap.TUNGraph.New()
 	for index, row in c.iterrows():
-		if G.IsNode(row['CanNodeId']) is False:
-			G.AddNode(row['CanNodeId'])
+		if G.IsNode(row['NId']) is False:
+			G.AddNode(row['NId'])
 		if G.IsNode(row['ComNodeId']) is False:
 			G.AddNode(row['ComNodeId'])
-		if G.IsEdge(row['CanNodeId'], row['ComNodeId']) is False:
-			G.AddEdge(row['CanNodeId'], row['ComNodeId'])
+		if G.IsEdge(row['NId'], row['ComNodeId']) is False:
+			G.AddEdge(row['NId'], row['ComNodeId'])
 
 	print "G node count is %d" % (G.GetNodes())
 	print "G edge count is %d" % (G.GetEdges())
 
 	GraphClustCoeff = snap.GetClustCf(G, -1)
-	print "Clustering coefficient: %f" % (GraphClustCoeff)
+	print "Clustering coefficient: %f" % (GraphClustCoeff) ## TODO: debug, this value = 0, seems wrong
 
 	x, y = getDataPointsToPlot(G)
 	plt.loglog(x, y, linestyle = 'dotted', color = 'b', label = '2008 - 2016 Campaign Financial Network')
@@ -171,5 +235,4 @@ if __name__ == "__main__":
 	plt.legend()
 	plt.show()
 
-	snap.SaveEdgeList(G, "../data/campaignNetworks.txt", "Save 2008 to 2016 campaign network info as tab-separated list of edges")
-	##TODO: use bb's node IDs
+	snap.SaveEdgeList(G, "../processed-data/campaignNetworks.txt", "Save 2008 to 2016 campaign network info as tab-separated list of edges, using unified candidate node IDs")
